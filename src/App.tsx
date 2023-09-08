0a5db69717b2cf41743ada21b4b0b68d39328209
dnlda <72078412+dnlda@users.noreply.github.com>
2023-09-09 01:26:56 +0300

    Create ToDo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { TodoList } from './TodoList';
+import { AddTodoForm } from './AddTodoForm';
+
+const initialTodos: Todo[] = [
+  {
+    text: "Walk the dog",
+    complete: false,
+  },
+  {
+    text: "Write app",
+    complete: true,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(() => {
+    // Check if todos exist in local storage
+    const storedTodos = localStorage.getItem('todos');
+    return storedTodos ? JSON.parse(storedTodos) : initialTodos;
+  });
+  const [filter, setFilter] = useState("All");
+
+  useEffect(() => {
+    //Save todos to local storage whenever it changes
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo: any) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (text: string) => {
+    const newTodo = { text, complete: false };
+    setTodos([...todos, newTodo]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <TodoList 
+        todos={todos.filter((todo: any) => {
+          if (filter === "All") {
+            return true;
+          } else if (filter === "Complete") {
+            return todo.complete;
+          } else if (filter === "InProgress") {
+            return !todo.complete;
+          }
+          return false;
+        })} 
+        toggleTodo={toggleTodo} 
+      />
+      <AddTodoForm 
+        addTodo={addTodo}
+      />
+      <div>
+        <button 
+          onClick={() => setFilter("All")}
+          style={{ marginRight: "10px" }}  
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          All
+        </button>
+        <button
+          onClick={() => setFilter("Complete")}
+          style={{ marginRight: "10px" }}
+        >
+          Copmlete
+        </button>
+        <button
+          onClick={() => setFilter("InProgress")}
+          style={{ marginRight: "10px" }}
+        >
+          InProgress
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default App;
